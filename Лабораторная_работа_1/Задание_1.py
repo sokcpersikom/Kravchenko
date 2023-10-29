2064034bbbf08e00df4198f31d4dd3858039f93f
sokcpersikom <149332231+sokcpersikom@users.noreply.github.com>
2023-10-29 23:26:26 +0300

    Add files via upload

---
+++ Лабораторная_работа_1/Задание_1.py
@@ -0,0 +1,14 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+numbers[4] = ( numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[5] + numbers[6]
+               + numbers[7] + numbers[8] + numbers[9] + numbers[10] + numbers[11] + numbers[12]
+               + numbers[13] + numbers[14] + numbers[15] + numbers[16] + numbers[17] + numbers[18]
+               + numbers[19]) / 20
+
+lok = numbers
+
+print("Измененный список:", lok)
+
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+
---
+++ Лабораторная_работа_1/Задание_4.py
@@ -0,0 +1,14 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+
+users_2 = {'user1', 'user2', 'user3', 'user1', 'user4', 'user2'}
+
+dict_users = {
+    "Общее количество": 0,
+    "Уникальные посещения": 0,
+}
+
+dict_users["Общее количество"] += len(users)
+dict_users["Уникальные посещения"] += len(users_2)
+
+print(dict_users)
+# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
