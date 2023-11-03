72155ca68c912bd768fbebcb6be16042131d7e6f
София <149332231+sokcpersikom@users.noreply.github.com>
2023-11-03 12:32:10 +0300

    Update Задание_1.py

--- Лабораторная_работа_1/Задание_1.py
+++ Лабораторная_работа_1/Задание_1.py
@@ -1,11 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers[4] = ( numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[5] + numbers[6]
-               + numbers[7] + numbers[8] + numbers[9] + numbers[10] + numbers[11] + numbers[12]
-               + numbers[13] + numbers[14] + numbers[15] + numbers[16] + numbers[17] + numbers[18]
-               + numbers[19]) / 20
-
 lok = numbers
+tib = len(numbers)
+kih = sum(numbers[:4]) + sum(numbers[5:])
+
+numbers[4] = kih / tib
 
 print("Измененный список:", lok)
 
